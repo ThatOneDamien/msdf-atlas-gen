69cf6b6a2e8e1aefc94792d97b127e9fd47536c0
Damien Wilson <damienjw2104@gmail.com>
2024-06-28 20:02:51 -0500

    Removed all tabs

--- premake5.lua
+++ premake5.lua
@@ -1,93 +1,93 @@
 project "msdfgen"
-	kind "StaticLib"
+    kind "StaticLib"
     location "msdfgen"
-	language "C++"
-	cppdialect "C++11"
+    language "C++"
+    cppdialect "C++11"
     staticruntime "on"
 
-	targetdir ("%{wks.location}/bin/" .. outdir .. "/%{prj.name}")
-	objdir ("%{wks.location}/bin/int/" .. outdir .. "/%{prj.name}")
+    targetdir ("%{wks.location}/bin/" .. outdir .. "/%{prj.name}")
+    objdir ("%{wks.location}/bin/int/" .. outdir .. "/%{prj.name}")
 
-	files
-	{
-		"msdfgen/core/**.h",
-		"msdfgen/core/**.hpp",
-		"msdfgen/core/**.cpp",
-		"msdfgen/ext/**.h",
-		"msdfgen/ext/**.hpp",
-		"msdfgen/ext/**.cpp",
-		"msdfgen/lib/**.cpp",
-		"msdfgen/include/**.h"
-	}
+    files
+    {
+        "msdfgen/core/**.h",
+        "msdfgen/core/**.hpp",
+        "msdfgen/core/**.cpp",
+        "msdfgen/ext/**.h",
+        "msdfgen/ext/**.hpp",
+        "msdfgen/ext/**.cpp",
+        "msdfgen/lib/**.cpp",
+        "msdfgen/include/**.h"
+    }
 
-	includedirs
-	{
-		"msdfgen/include",
-		"../FreeType/include"
-	}
+    includedirs
+    {
+        "msdfgen/include",
+        "../FreeType/include"
+    }
 
-	defines
-	{
-		"MSDFGEN_USE_CPP11"
-	}
+    defines
+    {
+        "MSDFGEN_USE_CPP11"
+    }
 
-	links
-	{
-		"FreeType"
-	}
+    links
+    {
+        "FreeType"
+    }
 
-	filter "system:windows"
-		systemversion "latest"
+    filter "system:windows"
+        systemversion "latest"
 
-	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
 
-	filter "configurations:Release"
-		runtime "Release"
-		optimize "on"
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
 
 project "msdf-atlas-gen"
-	kind "StaticLib"
-	language "C++"
-	cppdialect "C++17"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++17"
     staticruntime "on"
 
-	targetdir ("%{wks.location}/bin/" .. outdir .. "/%{prj.name}")
-	objdir ("%{wks.location}/bin/int/" .. outdir .. "/%{prj.name}")
+    targetdir ("%{wks.location}/bin/" .. outdir .. "/%{prj.name}")
+    objdir ("%{wks.location}/bin/int/" .. outdir .. "/%{prj.name}")
 
-	files
-	{
-		"msdf-atlas-gen/**.h",
-    	"msdf-atlas-gen/**.hpp",
-    	"msdf-atlas-gen/**.cpp"
-	}
+    files
+    {
+        "msdf-atlas-gen/**.h",
+        "msdf-atlas-gen/**.hpp",
+        "msdf-atlas-gen/**.cpp"
+    }
 
-	includedirs
-	{
-		"msdf-atlas-gen",
+    includedirs
+    {
+        "msdf-atlas-gen",
         "artery-font-format",
-		"msdfgen",
-		"msdfgen/include"
-	}
+        "msdfgen",
+        "msdfgen/include"
+    }
 
-	defines
-	{
-		"_CRT_SECURE_NO_WARNINGS"
-	}
+    defines
+    {
+        "_CRT_SECURE_NO_WARNINGS"
+    }
 
-	links
-	{
-		"msdfgen"
-	}
+    links
+    {
+        "msdfgen"
+    }
 
-	filter "system:windows"
-		systemversion "latest"
+    filter "system:windows"
+        systemversion "latest"
 
-	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
 
-	filter "configurations:Release"
-		runtime "Release"
-		optimize "on"
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
